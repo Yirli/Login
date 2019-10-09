5624ade38bcd40e9231a7092176082eac21917b9
Andres Operador <aobm98@gmail.com>
2019-10-09 17:05:42 -0600

    Validaciones en Lab1
    Se dan las validaciones en el creado por yir

--- Lab/partials/signup.php
+++ Lab/partials/signup.php
@@ -5,7 +5,20 @@
   
   $message = '';
 
-  if (!empty($_POST['email']) && !empty($_POST['password'])) {
+
+  if (empty($_POST['email'])) {
+    $message = 'Please enter your email.';
+  }elseif(!isValidPassword($_POST['password'])){
+    $message = 'Password is not strong enough. You have to include 8 characters, at least 1 Uppercase, 1 Lowercase and 1 number.';
+  }elseif(empty($_POST['password'])){
+    $message = 'Please enter your password.';
+  }elseif (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    $message = "Invalid email format";
+  }elseif(empty($_POST['name']) || empty($_POST['lastName']) || empty($_POST['sLastName'])){
+    $message = 'Please provide your full name.';
+  }elseif(empty($_POST['username'])){
+    $message = 'Your username is not valid.';
+  }else{
     $sql = "INSERT INTO users (email, password,name,lastName,sLastName,username,cellphone,phone,phoneCode) VALUES (:email,:password,:name,:lastName,:sLastName,:username,:cellphone,:phone,:phoneCode)";
     $stmt = $conn->prepare($sql);
     $stmt->bindParam(':email', $_POST['email']);
@@ -25,7 +38,24 @@
     } else {
       $message = 'Sorry there must have been an issue creating your account';
     }
+  } 
+
+  function isValidPassword($password){
+    $uppercase = preg_match('@[A-Z]@', $password);
+    $lowercase = preg_match('@[a-z]@', $password);
+    $number    = preg_match('@[0-9]@', $password);
+
+    if(!$uppercase || !$lowercase || !$number || strlen($password) < 8) {
+      return false;
+      echo 'Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.';
+    }else{
+      return true;
+      echo 'Strong password.';
+    }
+
   }
+
+  echo $message;
 ?>
 
 
