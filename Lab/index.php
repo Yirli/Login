ec45f22ad190b54f4831c9873a4acad1be2f9e34
Yirli <yirli.9527@gmail.com>
2019-10-13 17:03:54 -0600

    Add files via upload

--- Lab/index.php
+++ Lab/index.php
@@ -1,9 +1,9 @@
 <?php
   session_start();
 
-  require 'partials/database.php';
+  //require 'database.php';
 
-  if (isset($_SESSION['user_id'])) {
+ /* if (isset($_SESSION['user_id'])) {
     $records = $conn->prepare('SELECT id, email, password FROM users WHERE id = :id');
     $records->bindParam(':id', $_SESSION['user_id']);
     $records->execute();
@@ -14,7 +14,7 @@
     if (count($results) > 0) {
       $user = $results;
     }
-  }
+  }*/
 ?>
 
 <!DOCTYPE html>
@@ -26,19 +26,6 @@
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    <?php require 'partials/header.php' ?>
-
-    <?php if(!empty($user)): ?>
-      <br> Welcome. <?= $user['email']; ?>
-      <br>You are Successfully Logged In
-      <a href="logout.php">
-        Logout
-      </a>
-    <?php else: ?>
-      <h1>Please Login or SignUp</h1>
-
-      <a href="login.php">Login</a> or
-      <a href="signup.php">SignUp</a>
-    <?php endif; ?>
+    <h1>Bienvenido</h1>
   </body>
 </html>
